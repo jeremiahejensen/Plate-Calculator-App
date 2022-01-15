af57d6cec8f797750cf8cdeee93f798b88ee7f7e
Tommy Jones <tommyljones4@gmail.com>
2022-01-15 15:20:35 -0600

    Added Color to Plates

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { API, Amplify, graphqlOperation} from 'aws-amplify';
 import { createPlates, newPlate } from './graphql/mutations';
 import { withAuthenticator } from '@aws-amplify/ui-react';
@@ -8,39 +9,59 @@ import './App.css';
 
 Amplify.configure(awsExports);
 
-//API.graphql(graphqlOperation(createPlates, {input: newPlate}));
+const initialFormState = { weight: 0, color: '#000000' }
+
+
 
 function App({ isPassedToWithAuthenticator, signOut, user }) {
-// if (!isPassedToWithAuthenticator) {
-  // throw new Error(`isPassedToWithAuthenticator was not provided`);
-  //}
-
-
-
   
+  const [weights, setWeights] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+  
+  //This is the function that writes to the database
   async function createPlates() {
     const newPlate = { 
-      Weight: 45,
+      Weight: formData.weight,
       description: "hello",
       username: user.username,
-      color: '000000'
+      color: formData.color
   };
     await API.graphql({ query: createPlatesMutation, variables: { input: newPlate } });
   }
 
-
   return (
-    <>
-      <button onClick={createPlates}>Create Plate</button>
+    <div className="App">
+   
       <h1>Hello {user.username}</h1>
+      <label for="color">Color:</label>
+      <p>Choose your weight's colors:</p>
+<div>
+    <input type="color" id="color" name="color"
+    onChange={e => setFormData({ ...formData, 'color': e.target.value})}
+    placeholder="Color"
+    value={formData.color} 
+         />
+      </div>
+      <br/>
+      <label for="weight">Weight (LBS):</label>
+      <input
+        onChange={e => setFormData({ ...formData, 'weight': e.target.value})}
+        placeholder="Weight"
+        value={formData.weight}
+      />
+      <br/>
+      <button onClick={createPlates}>Create Plate</button>
+      <br/> 
       <button onClick={signOut}>Sign out</button>
-    </>
+      </div>
   );
 }
 
-export default withAuthenticator(App);
-//await API.graphql(graphqlOperation(createPlates, {input: todo}));
 
+
+
+
+export default withAuthenticator(App);
 export async function getStaticProps() {
   return {
     props: {
